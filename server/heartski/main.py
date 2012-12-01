5283ada7b7e704ef51c4f82fbeff56b8cb7005bf
Christy O'Reilly <christy@oreills.co.uk>
2012-12-01 15:16:14 +0000

    Serve static files

--- server/heartski/main.py
+++ server/heartski/main.py
@@ -2,28 +2,30 @@ import os
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 
-participants = set()
+players = set()
 PORT = 8000
 
-def hello_world(environ, start_response):
-    ws = environ["wsgi.websocket"]
-    participants.add(ws)
+def ski_ws_handler(ws):
     try:
+        players.add(ws)
         while True:
             m = ws.receive()
-            for p in participants:
+            for p in players:
                 p.send(m)
     finally:
-        participants.remove(ws)
+        players.remove(ws)
 
 def dispatch(environ, start_response):
     """Resolves to the web page or the websocket depending on the path."""
-    if environ['PATH_INFO'] == '/ski':
-        return hello_world(environ, start_response)
-    else:
-        start_response('200 OK', [('content-type', 'text/html')])
-        html_path = os.path.join(os.path.dirname(__file__), 'websocket_chat.html')
-        return [open(html_path).read() % {'port': PORT}]
+    path = environ['PATH_INFO']
+    if path == '/ski':
+        return ski_ws_handler(environ["wsgi.websocket"])
+    if path.startswith('/'):
+        path = path[1:]
+    path = path or 'index.html'
+    start_response('200 OK', [('content-type', 'text/html')])
+    html_path = os.path.join(os.path.dirname(__file__), '../../www', path)
+    return open(html_path).read()
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(("", PORT), dispatch,
