a78b7d581ec27a01590c41925b362a3ec146fbcd
Gehan Gonsalkorale <gehan.g@gmail.com>
2012-12-01 16:27:11 +0000

    Add cake pit

--- coffeescript/PlayScreen.coffee
+++ coffeescript/PlayScreen.coffee
@@ -1,11 +1,31 @@
-`define(function(){`
+`define(['./CakePit'], function(CakePit){`
+
+counter = 0
+base = me.ObjectSettings
+base.name = "CakePit"
 
 PlayScreen = me.ScreenObject.extend
+
     onResetEvent: ->
         me.levelDirector.loadLevel 'area01'
 
-    onDestroyEvent: ->
+        cake = new CakePit(100, 100, base)
+        me.game.add(cake, 1000)
 
+
+    update: ->
+        #console.log 'update'
+
+    draw: ->
+        #console.log 'draw'
+        counter += 1
+        if counter % 20 == 0
+            console.log 'draw ', counter
+            cake = new CakePit(counter * 10, 100, base)
+            me.game.add(cake, 1000)
+            me.game.sort()
+
+    onDestroyEvent: ->
 
 return PlayScreen
 `})`
--- www/media/js/PlayScreen.js
+++ www/media/js/PlayScreen.js
@@ -1,11 +1,31 @@
 (function() {
-  define(function(){;
+  define(['./CakePit'], function(CakePit){;
 
-  var PlayScreen;
+  var PlayScreen, base, counter;
+
+  counter = 0;
+
+  base = me.ObjectSettings;
+
+  base.name = "CakePit";
 
   PlayScreen = me.ScreenObject.extend({
     onResetEvent: function() {
-      return me.levelDirector.loadLevel('area01');
+      var cake;
+      me.levelDirector.loadLevel('area01');
+      cake = new CakePit(100, 100, base);
+      return me.game.add(cake, 1000);
+    },
+    update: function() {},
+    draw: function() {
+      var cake;
+      counter += 1;
+      if (counter % 20 === 0) {
+        console.log('draw ', counter);
+        cake = new CakePit(counter * 10, 100, base);
+        me.game.add(cake, 1000);
+        return me.game.sort();
+      }
     },
     onDestroyEvent: function() {}
   });
